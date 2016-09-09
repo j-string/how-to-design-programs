53e5cf374560f9db5d3083d6a691d6d7ed2af042
j-string <justin.michael.stringer@gmail.com>
2016-09-09 10:15:24 -0500

    section 2.2 exercises

---
+++ section-2/e2.2.3-triangle.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname e2.2.3-triangle) (read-case-sensitive #t) (teachpacks ((lib "convert.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "convert.rkt" "teachpack" "htdp")) #f)))
+; Consumes the base and heighth of a triangle and produces its area
+
+(define (triangle b h)
+  (/ (* b h) 2))
